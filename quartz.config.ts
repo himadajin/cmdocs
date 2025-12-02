1baa74be62b68ad56a53ba56683f20393e71ec2a
Taiki Harada <haradataiki19990927@gmail.com>
2025-12-02 21:52:11 +0900

    feat: add GitHub Pages deployment workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "himadajin.github.io/cmdocs",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
